5c2475e25f17709580cdffe562bd3046864a05ca
eroslp99 <eroslp99@gmail.com>
2022-01-16 10:21:08 +0800

    fix bugs  20220116 102108

--- cnblogPost.js
+++ cnblogPost.js
@@ -91,7 +91,7 @@ async function main() {
     //console.log(await sqlite.open('./freeok.db'))
     const page = await browser.newPage()
     //await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36')
-    await page.authenticate({username:setup.proxy.usr, password:setup.proxy.pwd});
+    await page.authenticate({username:setup.proxy.usr, password:setup.proxy.pwd})
     page.on('dialog', async dialog => {
         //console.info(`➞ ${dialog.message()}`);
         await dialog.dismiss();
